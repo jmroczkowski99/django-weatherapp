a9a077b1fa80d626a108646be6167bf255a3d34d
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-13 20:10:45 +0100

    feat: update index view

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.conf import settings
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    api_key = settings.OPENWEATHER_API_KEY
+    city = "Gryfice"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    city_weather = requests.get(url).json()
+
+    weather = {
+        "city": city,
+        "temperature": city_weather["main"]["temp"],
+        "sensed_temperature": city_weather["main"]["feels_like"],
+        "description": city_weather["weather"][0]["description"],
+        "icon": city_weather["weather"][0]["icon"],
+    }
+
+    return render(request, "index.html", {"weather": weather})
