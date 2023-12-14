3827f86c6824d6c2a0dc7534b3222d26e02a2242
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-14 19:25:24 +0100

    refactor: change city variable to city_name

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -6,13 +6,13 @@ import requests
 
 def index(request):
     api_key = settings.OPENWEATHER_API_KEY
-    city = "Gryfice"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    city_name = "Gryfice"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
 
     city_weather = requests.get(url).json()
 
     weather = {
-        "city": city,
+        "city_name": city_name,
         "temperature": city_weather["main"]["temp"],
         "sensed_temperature": city_weather["main"]["feels_like"],
         "description": city_weather["weather"][0]["description"],
