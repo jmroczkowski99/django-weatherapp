b20bded478140a981313b9df0dd0ddc85c746d00
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-16 18:43:12 +0100

    feat: make all city names format the same way

--- weather/weatherapp/models.py
+++ weather/weatherapp/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+
 class City(models.Model):
     name = models.CharField(max_length=200)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
