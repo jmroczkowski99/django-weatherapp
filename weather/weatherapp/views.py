913f1d3044236032044a5cf7dc58670978a76dc6
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-16 18:44:36 +0100

    feat: rebuild index view, include form, error msgs

--- weather/weatherapp/views.py
+++ weather/weatherapp/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.conf import settings
 import requests
+from .models import City
+from .forms import CityForm
 
 
 def index(request):
     api_key = settings.OPENWEATHER_API_KEY
-    city_name = "Gryfice"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units=metric"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
+    error_message = ""
 
-    city_weather = requests.get(url).json()
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city_name = form.cleaned_data["name"]
+            if City.objects.filter(name=city_name).count() == 0:
+                r = requests.get(url.format(city_name, api_key)).json()
+                if r["cod"] == 200:
+                    form.save()
+                else:
+                    error_message = "This city doesn't exist in our database :("
+            else:
+                error_message = "This city is already in our database"
+    else:
+        form = CityForm()
 
-    weather = {
-        "city_name": city_name,
-        "temperature": city_weather["main"]["temp"],
-        "sensed_temperature": city_weather["main"]["feels_like"],
-        "description": city_weather["weather"][0]["description"],
-        "icon": city_weather["weather"][0]["icon"],
-    }
+    city_names = City.objects.all()
+    weather_data = []
 
-    return render(request, "index.html", {"weather": weather})
+    for city_name in city_names:
+        city_weather = requests.get(url.format(city_name, api_key)).json()
+        weather = {
+            "city_name": city_name,
+            "temperature": city_weather["main"]["temp"],
+            "sensed_temperature": city_weather["main"]["feels_like"],
+            "description": city_weather["weather"][0]["description"],
+            "icon": city_weather["weather"][0]["icon"],
+        }
+        weather_data.append(weather)
+
+    return render(request, "index.html", {"weather_data": weather_data, "form": form, "error_message": error_message})
