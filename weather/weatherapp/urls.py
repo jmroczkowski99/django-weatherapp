76c9f389de2ce8914f3c7d83c3cb3c6518de43ca
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-20 22:35:09 +0100

    fix: delete frontslash in delete_city url

--- weather/weatherapp/urls.py
+++ weather/weatherapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="homepage"),
-    path("/delete/<str:city_name>/", views.delete_city, name="delete_city"),
+    path("delete/<str:city_name>/", views.delete_city, name="delete_city"),
 ]
