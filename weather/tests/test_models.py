7947156e20ffce137cbcdb401d222b9549bdf233
jmroczkowski99 <j.mroczkowski1999@gmail.com>
2023-12-20 21:55:26 +0100

    test: add model save method test

--- weather/tests/test_models.py
+++ weather/tests/test_models.py
@@ -10,3 +10,7 @@ class TestPostModel:
     def test_str_return(self):
         new_city = City.objects.create(name="Gryfice")
         assert new_city.name == "Gryfice"
+
+    def test_save(self):
+        new_city = City.objects.create(name="los angeles")
+        assert new_city.name == "Los Angeles"
